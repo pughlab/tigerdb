4d9a034abcb1b9d1b2e555594414d3d2e2c5ffb7
mdpham <martindopham@gmail.com>
2022-08-04 01:49:25 -0400

    uncomment data explore page

--- ui/src/components/pages/Explore/index.tsx
+++ ui/src/components/pages/Explore/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Message, Divider, List, Container, Input, Segment, Form, Button } from 'semantic-ui-react'
 import DataVariableTable from '../../tables/DataVariableTable'
 
-import {gql, useQuery} from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 import { CSVLink } from "react-csv";
 
 import SunburstVisualization from '../../visualizations/sunburst/SunburstVisualization';
@@ -10,15 +10,15 @@ import TreemapVisualization from '../../visualizations/treemap/TreemapVisualizat
 
 
 
-function DownloadDataVariables ({data}) {
+function DownloadDataVariables({ data }) {
     console.log(data)
     // Can combine with react-table headers
     const headers = [
-        {label: 'chromosome', key: 'chromosome'},
-        {label: 'start', key: 'start'},
-        {label: 'end', key: 'end'},
-        {label: 'datavalue', key: 'datavalue'}
-      ]
+        { label: 'chromosome', key: 'chromosome' },
+        { label: 'start', key: 'start' },
+        { label: 'end', key: 'end' },
+        { label: 'datavalue', key: 'datavalue' }
+    ]
     return (
         <CSVLink data={data} headers={headers} filename={"plbr_export.csv"}>
             <Button fluid content={`Download ${data.length} variables`} />
@@ -26,11 +26,11 @@ function DownloadDataVariables ({data}) {
     )
 }
 
-export default function Explore () {
+export default function Explore() {
     const [searchText, setSearchText] = useState('')
     const [start, setStart] = useState(0)
     const [end, setEnd] = useState(1000)
-    const {data, loading, error} = useQuery(gql`
+    const { data, loading, error } = useQuery(gql`
         query DataVariablesSearch($searchText: String!, $start: Int!, $end: Int!) {
             dataVariables(where: {
                 chromosome_CONTAINS: $searchText
@@ -44,25 +44,25 @@ export default function Explore () {
                 datavalue
             }
         }`,
-        {variables: {searchText, start, end}})
+        { variables: { searchText, start, end } })
     console.log(data)
 
     return (
         <>
             <Message>
-            Some text about data variables and searching to create visualizations
+                Some text about data variables and searching to create visualizations
 
-            <Divider horizontal />
-          </Message>
-              
-            {/* <Form as={Segment} attached='top'>
+                <Divider horizontal />
+            </Message>
+
+            <Form as={Segment} attached='top'>
                 <Form.Group widths={3}>
                     <Form.Field
                         control={Input}
                         label='Chromosome'
                         placeholder='Enter chromosome of interest'
                         value={searchText}
-                        onChange={(e, {value}) => setSearchText(value)}
+                        onChange={(e, { value }) => setSearchText(value)}
                     />
                     <Form.Field
                         control={Input}
@@ -71,7 +71,7 @@ export default function Explore () {
                         label='Start'
                         placeholder='Integer'
                         value={start}
-                        onChange={(e, {value}) => setStart(Math.max(0, parseInt(value)))}
+                        onChange={(e, { value }) => setStart(Math.max(0, parseInt(value)))}
                     />
                     <Form.Field
                         control={Input}
@@ -80,22 +80,21 @@ export default function Explore () {
                         label='End'
                         placeholder='Enter some terms of interest'
                         value={end}
-                        onChange={(e, {value}) => setEnd(Math.max(0, parseInt(value)))}
+                        onChange={(e, { value }) => setEnd(Math.max(0, parseInt(value)))}
                     />
                 </Form.Group>
-            </Form> */}
-          <Segment attached='bottom' loading={loading}>
-              
-            {/* {
-                !!data?.dataVariables && (
-                    <>
-                        <DownloadDataVariables data={data.dataVariables} />
-                        <DataVariableTable data={data.dataVariables} />
-                    </>
-                )
-                
-            } */}
-          </Segment>
+            </Form>
+            <Segment attached='bottom' loading={loading}>
+                {
+                    !!data?.dataVariables && (
+                        <>
+                            <DownloadDataVariables data={data.dataVariables} />
+                            <DataVariableTable data={data.dataVariables} />
+                        </>
+                    )
+
+                }
+            </Segment>
         </>
     )
 }
