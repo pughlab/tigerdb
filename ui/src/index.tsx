d816304ec14b4e163e7d208d7a22530bb527af55
mdpham <martindopham@gmail.com>
2022-08-04 01:15:07 -0400

    change react renderer for v18

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 import App from './App';
 
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
 
-render(<App />, document.getElementById('app'));
+// render(<App />, document.getElementById('app'));
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
