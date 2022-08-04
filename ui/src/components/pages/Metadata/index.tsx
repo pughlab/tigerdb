292568d83d2dadf1a9cb69164ca9e895a100f42a
mdpham <martindopham@gmail.com>
2022-08-04 03:37:26 -0400

    use routes in metadata page

--- ui/src/components/pages/Metadata/index.tsx
+++ ui/src/components/pages/Metadata/index.tsx
@@ -1,22 +1,43 @@
 import React, { useState } from 'react'
 import HierarchicalGraphVisualization from '../../visualizations/graph/hierarchical/HierarchicalGraphVisualization'
-import { Message, Divider, Form, Container } from 'semantic-ui-react'
+import { Message, Divider, Form, Container, List, Segment, Input } from 'semantic-ui-react'
+import { Routes, Route } from 'react-router-dom'
+import useRouter from '../../../hooks/useRouter'
 
-
-export default function Metadata ({}) {
+function OntologyDetails ({}) {
     return (
-        <>
-        <Container >
-        <Message>
-            Some text about data variables and searching to create visualizations
-
-        <Divider horizontal />
-        </Message>
-        <Form>
-
-        </Form>
         <HierarchicalGraphVisualization />
-        </Container>
-        </>
+    )
+}
+
+export default function Metadata({ }) {
+    const {navigate} = useRouter()
+    return (
+        <Routes>
+            <Route index element={(
+                <>
+                    <Container>
+                        <Message>
+                            Ontologies and metadata
+                            <Divider horizontal />
+                        </Message>
+
+                        <Form as={Segment}>
+                            <Form.Field
+                                control={Input}
+                                label='Search ontologies'
+                                placeholder='Enter some terms of interest'
+                            />
+                        </Form>
+
+                        <List relaxed='very' selection size='large'>
+                            <List.Item content='ontology' onClick={(e, d) => navigate('someOntologyID')} />
+                        </List>
+                    </Container>
+                </>
+            )} />
+
+            <Route path=':ontologyID' element={<OntologyDetails />} />
+        </Routes>
     )
 }
