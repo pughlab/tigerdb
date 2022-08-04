ec2f1af39f5054da64e68b7aea0834f68487b49a
mdpham <martindopham@gmail.com>
2022-08-04 01:16:26 -0400

    pie visx visualization

---
+++ ui/src/components/visualizations/pie/PieVisualization.tsx
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import Pie, { ProvidedProps, PieArcDatum } from '@visx/shape/lib/shapes/Pie';
+import { scaleOrdinal } from '@visx/scale';
+import { Group } from '@visx/group';
+import letterFrequency, { LetterFrequency } from '@visx/mock-data/lib/mocks/letterFrequency';
+import { animated, useTransition, to } from '@react-spring/web';
+import {ParentSize} from '@visx/responsive'
+import { Segment } from 'semantic-ui-react';
+
+
+const letters: LetterFrequency[] = letterFrequency.slice(0, 4);
+
+// accessor functions
+const frequency = (d: LetterFrequency) => d.frequency;
+
+const getLetterFrequencyColor = scaleOrdinal({
+  domain: letters.map((l) => l.letter),
+  range: ["#FE938C", "#E6B89C", "#EAD2AC", "#9CAFB7", "#4281A4"],
+});
+
+const defaultMargin = { top: 20, right: 20, bottom: 20, left: 20 };
+
+export type PieProps = {
+  width: number;
+  height: number;
+  margin?: typeof defaultMargin;
+  animate?: boolean;
+};
+
+function PieSVG ({
+  width,
+  height,
+  margin = defaultMargin,
+  animate = true,
+}: PieProps) {
+
+  if (width < 10) return null;
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  const radius = Math.min(innerWidth, innerHeight) / 2;
+  const centerY = innerHeight / 2;
+  const centerX = innerWidth / 2;
+  const donutThickness = 50;
+
+  return (
+    <svg width={width} height={height}>
+      <Group top={centerY + margin.top} left={centerX + margin.left}>
+        <Pie
+          data={letters}
+          pieValue={frequency}
+          pieSortValues={() => -1}
+          outerRadius={radius - donutThickness * 1.3}
+        >
+          {(pie) => (
+            <AnimatedPie<LetterFrequency>
+              {...pie}
+              animate={animate}
+              getKey={({ data: { letter } }) => letter}
+              onClickDatum={({ data } ) => console.log(data)}
+              getColor={({ data: { letter } }) => getLetterFrequencyColor(letter)}
+            />
+          )}
+        </Pie>
+      </Group>
+    </svg>
+  );
+}
+
+// react-spring transition definitions
+type AnimatedStyles = { startAngle: number; endAngle: number; opacity: number };
+
+const fromLeaveTransition = ({ endAngle }: PieArcDatum<any>) => ({
+  // enter from 360° if end angle is > 180°
+  startAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
+  endAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
+  opacity: 0,
+});
+const enterUpdateTransition = ({ startAngle, endAngle }: PieArcDatum<any>) => ({
+  startAngle,
+  endAngle,
+  opacity: 1,
+});
+
+type AnimatedPieProps<Datum> = ProvidedProps<Datum> & {
+  animate?: boolean;
+  getKey: (d: PieArcDatum<Datum>) => string;
+  getColor: (d: PieArcDatum<Datum>) => string;
+  onClickDatum: (d: PieArcDatum<Datum>) => void;
+  delay?: number;
+};
+
+function AnimatedPie<Datum>({
+  animate,
+  arcs,
+  path,
+  getKey,
+  getColor,
+  onClickDatum,
+}: AnimatedPieProps<Datum>) {
+  const transitions = useTransition<PieArcDatum<Datum>, AnimatedStyles>(arcs, {
+    from: animate ? fromLeaveTransition : enterUpdateTransition,
+    enter: enterUpdateTransition,
+    update: enterUpdateTransition,
+    leave: animate ? fromLeaveTransition : enterUpdateTransition,
+    keys: getKey,
+  });
+  return transitions((props, arc, { key }) => {
+    const [centroidX, centroidY] = path.centroid(arc);
+    const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
+
+    return (
+      <g key={key}>
+        <animated.path
+          // compute interpolated path d attribute from intermediate angle values
+          d={to([props.startAngle, props.endAngle], (startAngle, endAngle) =>
+            path({
+              ...arc,
+              startAngle,
+              endAngle,
+            }),
+          )}
+          fill={getColor(arc)}
+          onClick={() => onClickDatum(arc)}
+          onTouchStart={() => onClickDatum(arc)}
+        />
+        {hasSpaceForLabel && (
+          <animated.g style={{ opacity: props.opacity }}>
+            <text
+              fill="white"
+              x={centroidX}
+              y={centroidY}
+              dy=".33em"
+              fontSize={9}
+              textAnchor="middle"
+              pointerEvents="none"
+            >
+              {getKey(arc)}
+            </text>
+          </animated.g>
+        )}
+      </g>
+    );
+  });
+}
+
+export default function PieVisualization ({}) {
+    return (
+        <ParentSize>
+            {({ width, height }) => <PieSVG width={600} height={800} />}
+        </ParentSize>
+    )
+}
