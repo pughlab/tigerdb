30594a1c248ed858b034061f8285383c5d15071e
mdpham <martindopham@gmail.com>
2022-08-11 07:01:27 -0400

    raw dataset calendar heatmap

--- ui/src/components/visualizations/heatmap/calendar/CalendarHeatmapVisualization.tsx
+++ ui/src/components/visualizations/heatmap/calendar/CalendarHeatmapVisualization.tsx
@@ -1,55 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import ReactTooltip from 'react-tooltip';
 
 import 'react-calendar-heatmap/dist/styles.css';
 import './react-calendar-heatmap.css';
 
-const today = new Date();
+import { addDays, addMonths, addYears, format } from 'date-fns'
+import {atom, useAtom} from 'jotai'
+import { Button, Divider, Icon, Label, Segment } from 'semantic-ui-react';
 
-function shiftDate(date, numDays) {
-  const newDate = new Date(date);
-  newDate.setDate(newDate.getDate() + numDays);
-  return newDate;
-}
-
-function getRange(count) {
-  return Array.from({ length: count }, (_, i) => i);
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+import useRawDatasetCalendarHeatmapQuery from '../../../../hooks/useRawDatasetCalendarHeatmapQuery';
 
 export default function CalendarHeatmapVisualization() {
-    const randomValues = getRange(200).map(index => {
-      return {
-        date: shiftDate(today, -index),
-        count: getRandomInt(1, 3),
-      };
-    });
-    console.log(randomValues[randomValues.length-1])
-    return (
-      <div>
-        <CalendarHeatmap
-          startDate={shiftDate(today, -150)}
-          endDate={shiftDate(today, -1)}
-          values={randomValues}
-          classForValue={value => {
-            if (!value) {
-              return 'color-empty';
-            }
-            return `color-github-${value.count}`;
-          }}
-          tooltipDataAttrs={value => {
-            return {
-              'data-tip': `${value.date.toISOString().slice(0, 10)} has count: ${value.count}`,
-            };
-          }}
-          showWeekdayLabels={true}
-          onClick={value => alert(`Clicked on value with count: ${value.count}`)}
-        />
-        <ReactTooltip />
-      </div>
-    );
-  }
+  const {variables: {startDate, endDate, setEndDate}, heatmapData, loading} = useRawDatasetCalendarHeatmapQuery()
+
+  return (
+    <Segment loading={loading}>
+      <Divider horizontal>
+        <Button.Group>
+          <Button icon='left arrow' onClick={() => setEndDate(addMonths(endDate, -1))} />
+          <Button onClick={() => setEndDate(new Date())}>
+            <Icon name='calendar outline' />
+            <Label content={format(startDate, 'dd MMM yy')} />
+            -
+            <Label content={format(endDate, 'dd MMM yy')} />
+          </Button>
+          <Button icon='right arrow' onClick={() => setEndDate(addMonths(endDate, 1))}/>
+        </Button.Group>
+      </Divider>
+      {!!heatmapData &&
+          <CalendarHeatmap
+            startDate={startDate}
+            endDate={endDate}
+            values={heatmapData}
+            classForValue={value => !value ? 'color-empty' : `color-github-${value.count}`}
+            titleForValue={value => !!value ? `${value.date} has count: ${value.count}` : ''}
+            showWeekdayLabels={true}
+            weekdayLabels={['Su', 'M', 'T', 'W', 'Th', 'F', 'Sa']}
+            onClick={value => alert(`Clicked on value with count: ${value.count}`)}
+          />
+    }
+
+      <ReactTooltip />
+    </Segment>
+  );
+}
