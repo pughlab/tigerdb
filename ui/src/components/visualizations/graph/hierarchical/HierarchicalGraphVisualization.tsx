260a2a0c31f2fb6ae59f12835c4e55c7d99ac240
mdpham <martindopham@gmail.com>
2022-09-07 13:00:53 -0400

    hierarchical graph vis for ontology

--- ui/src/components/visualizations/graph/hierarchical/HierarchicalGraphVisualization.tsx
+++ ui/src/components/visualizations/graph/hierarchical/HierarchicalGraphVisualization.tsx
@@ -2,48 +2,48 @@ import React from 'react'
 import {GraphCanvas} from 'reagraph'
 import {ParentSize} from '@visx/responsive'
 
-const data = {
-  nodes: [
-    {
-      id: "n-1",
-      label: "1"
-    },
-    {
-      id: "n-2",
-      label: "2"
-    },
-    {
-      id: "n-3",
-      label: "3"
-    },
-    {
-      id: "n-4",
-      label: "4"
-    }
-  ],
-  edges: [
-    {
-      id: "1->2",
-      source: "n-1",
-      target: "n-2",
-      label: "Edge 1-2"
-    },
-    {
-      id: "1->3",
-      source: "n-1",
-      target: "n-3",
-      label: "Edge 1-3"
-    },
-    {
-      id: "1->4",
-      source: "n-1",
-      target: "n-4",
-      label: "Edge 1-4"
-    }
-  ],
-}
+// const data = {
+//   nodes: [
+//     {
+//       id: "n-1",
+//       label: "1"
+//     },
+//     {
+//       id: "n-2",
+//       label: "2"
+//     },
+//     {
+//       id: "n-3",
+//       label: "3"
+//     },
+//     {
+//       id: "n-4",
+//       label: "4"
+//     }
+//   ],
+//   edges: [
+//     {
+//       id: "1->2",
+//       source: "n-1",
+//       target: "n-2",
+//       label: "Edge 1-2"
+//     },
+//     {
+//       id: "1->3",
+//       source: "n-1",
+//       target: "n-3",
+//       label: "Edge 1-3"
+//     },
+//     {
+//       id: "1->4",
+//       source: "n-1",
+//       target: "n-4",
+//       label: "Edge 1-4"
+//     }
+//   ],
+// }
 
-function HierarchicalTopDown({width, height}) {
+function HierarchicalTopDown({width, height, data}) {
   return (
     <div style={{width: width, height: height}}>
       <GraphCanvas
@@ -56,10 +56,10 @@ function HierarchicalTopDown({width, height}) {
   )
 }
 
-export default function HierarchicalGraphVisualization () {
+export default function HierarchicalGraphVisualization ({data}) {
   return (
     <ParentSize>
-      {(size) => <HierarchicalTopDown width={600} height={800} />}
+      {(size) => <HierarchicalTopDown width={600} height={800} data={data} />}
     </ParentSize>
   )
 }
