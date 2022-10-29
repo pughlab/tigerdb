9584afe8e74b7f0a745677f6a9948c17894aa7cf
suluxan <suluxan.m@gmail.com>
2022-10-29 03:02:28 -0400

    create curated datasets using apoc iterate and apoc load csv (wip - needs to be updated to new api)

--- api/src/graphql/resolvers/curatedDataset.ts
+++ api/src/graphql/resolvers/curatedDataset.ts
@@ -1,4 +1,4 @@
-import { listBucketObjects } from '../../minio/minio'
+import { listBucketObjects, makePresignedURL } from '../../minio/minio'
 import papa from 'papaparse'
 import { ApolloError } from 'apollo-server'
 import zlib from 'zlib'
@@ -18,72 +18,20 @@ export const resolvers = {
         const { curatedDatasets: [curatedDataset] } = await CuratedDatasetModel.create({ input: [{ name, description }] })
         const { curatedDatasetID } = curatedDataset
 
-        const bucketItemNames = (await listBucketObjects(minioClient, bucketName)).map(({ name }) => name)
-        console.log(bucketItemNames)
 
-        // get last uploaded item in bucket (slice[-1][0]) and return stream 
-        const stream = await minioClient.getObject(bucketName, bucketItemNames.slice(-1)[0])
-        // gunzip stream
-        const compressedFileStream = stream.pipe(zlib.createGunzip())
+        const session = driver.session()
 
-        async function dataVariableTransformation() {
-          let result = { meta: {}, data: [] };
-          // let result = []
-          await new Promise((resolve, reject) => {
-            papa.parse(compressedFileStream, {
-              worker: true,
-              delimiter: " ",
-              step: (results) => {
-                result.data.push(results.data)
-              },
-              complete: () => {
-                resolve(result);
-              },
-              error: (err) => {
-                reject(err);
-              },
-            })
-          })
-          const chunkSize = 1000;
-          console.log(result.data.length)
-          // var i = 0, len = result.data.length
-          // while (i < len) {
-          
-          for (let i = 0; i < result.data.length; i += chunkSize) {
-          const chunk = result.data.slice(i, i + chunkSize);
-          console.log("NOW PROCESSING CHUNK: "+i)
-          // console.log(chunk)
-          await Promise.all(chunk.map(async (result) => {
-          // for (let i = 0; i < result.data.length; i += 4) {
-            //gathering all chr1, putting into array named 'chromosome'
-            const chromosome = result[0]
-            const start = result[1]
-            const end = result[2]
+        const bucketObjects = (await listBucketObjects(minioClient, bucketName)).map(({ name }) => name)
+        console.log(bucketObjects)
 
-            const datavalue = result[3]
+        const presignedURL = await makePresignedURL(minioClient, bucketName, bucketObjects.slice(-1)[0])
+        console.log(presignedURL)
 
-            const DataVariableModel = ogm.model("DataVariable");
+        const createCuratedDatasetFromRawDataset = await session.run(
+          "CALL apoc.periodic.iterate(\'CALL apoc.load.csv($presignedURL, {sep: \" \", compression: \"GZIP\"}) YIELD list\', \'MATCH (b:CuratedDataset {curatedDatasetID: $curatedDatasetID}) CREATE (a:DataVariable {dataVariableID: apoc.create.uuid(), chromosome: list[0], start: toInteger(list[1]), end: toInteger(list[2]), datavalue: toFloat(list[3]) }), (b)-[:HAS_DATA_VARIABLE]->(a) RETURN a\', { batchSize:10000, iterateList: true, parallel:true, params:{curatedDatasetID: $curatedDatasetID, presignedURL: $presignedURL}})",
+          {curatedDatasetID: curatedDatasetID, presignedURL: presignedURL}
+        )
 
-            const { dataVariables: [dataVariable] } = await DataVariableModel.create({ input: [{ chromosome: chromosome, start: parseInt(start), end: parseInt(end), datavalue: parseFloat(datavalue) }] })
-            const { dataVariableID, ...dataVariableRest } = dataVariable
-
-            await CuratedDatasetModel.update({
-              where: { curatedDatasetID },
-              update: {
-                dataVariables: {
-                  connectOrCreate: {
-                    where: { node: { dataVariableID } },
-                    onCreate: { node: dataVariableRest }
-                  }
-                }
-              }
-            })
-
-          }))
-          // i+=chunkSize
-          console.log("done neo4j transformation")
-        }}
-        dataVariableTransformation()
         return curatedDataset
       } catch (error) {
         console.log(error)
