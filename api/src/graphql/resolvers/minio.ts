41d1649c4f6832898d18339be06d8434d3a93173
jfoong <justin.foong@uhn.ca>
2023-03-03 14:57:19 -0500

    File validations

--- api/src/graphql/resolvers/minio.ts
+++ api/src/graphql/resolvers/minio.ts
@@ -2,11 +2,186 @@ import { minioClient, listBucketObjects } from '../../minio/minio'
 import { ApolloError } from 'apollo-server'
 import  zlib  from 'zlib';
 import { extname } from 'path'
+import papa from 'papaparse'
+import * as R from 'remeda'
 
+const validateFile = async (ogm, rawDatasetID, objectName, preview, header) => {
+
+  const RawDatasetModel = ogm.model("RawDataset")
+  const MinioUploadModel = ogm.model("MinioUpload")
+
+  const bucketName = `raw-dataset-${rawDatasetID}`
+
+  const rawDataset = await RawDatasetModel.find({ where: {rawDatasetID} })
+  const datafileMinioUpload = await MinioUploadModel.find( {where: {bucketName: bucketName, objectName: objectName}})
+
+  const stream = await minioClient.getObject(bucketName, objectName)
+
+  // gunzip stream
+  const compressedFileStream = stream.pipe(zlib.createGunzip())
+
+  async function getCSV() {
+    let result = []
+    await new Promise((resolve, reject) => {
+      papa.parse(compressedFileStream, {
+        worker: true,
+        header: header,
+        delimiter: ",",
+        preview: preview,
+        step: (results) => {
+          result.push(results.data)
+        },
+        complete: () => {
+          resolve(result);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      })
+    })
+
+    return {bucketName, rawDataset, datafileMinioUpload, result}
+  }
+
+  const result = await getCSV()
+
+  return result
+
+}
+
+const checkNoPrefix = (keys) => {
+    // keys.push('%permission_test1') // test invalid keys
+    // keys.push('%permission_test2') // test invalid keys
+
+    let toReturn
+
+    const invalid_keys = keys.filter((key) => key.startsWith('%permission_'))
+    const isValid = invalid_keys.length === 0
+    let message
+    
+    if (isValid) {
+      message = "File is valid"
+    } else {
+      message =`File is not valid. Data variables cannot start with '%permission_'. Invalid keys: ${invalid_keys}`
+    }
+
+    return {isValid, message}
+}
 
 export const resolvers = {
   Query: {
+    validateRawdatafile: async (obj, { rawDatasetID, objectName }, { driver, ogm }) => {
+      try {
 
+        const RawDatasetModel = ogm.model("RawDataset")
+
+        const {bucketName, rawDataset, datafileMinioUpload, result:csv} = await validateFile(ogm, rawDatasetID, objectName, 1, true)
+        let keys = Object.keys(csv[0])
+
+        const {isValid, message} = checkNoPrefix(keys)
+
+        const rawDatasetModelUpdate = await RawDatasetModel.update({ 
+          where: { rawDatasetID },
+          update: { rawdataFile: {connect: {
+            where: {node: {objectName: datafileMinioUpload[0].objectName}},
+            edge: {validated: isValid},
+          }}},
+        })
+
+        return {isValid, message}
+      } catch (error) {
+        console.log(error)
+        throw new ApolloError('query.validateRawdatafile error')
+      }
+    },
+    validateCodebook: async (obj, { rawDatasetID, objectName }, { driver, ogm }) => {
+      try {
+
+        const RawDatasetModel = ogm.model("RawDataset")
+
+        const {bucketName, rawDataset, datafileMinioUpload, result:csv} = await validateFile(ogm, rawDatasetID, objectName, 0, false)
+        let keys = csv.map(x => x[0])
+
+        const {isValid, message} = checkNoPrefix(keys)
+
+        const rawDatasetModelUpdate = await RawDatasetModel.update({ 
+          where: { rawDatasetID },
+          update: { codeBook: {connect: {
+            where: {node: {objectName: datafileMinioUpload[0].objectName}},
+            edge: {validated: isValid},
+          }}},
+        })
+
+        return {isValid, message}
+      } catch (error) {
+        console.log(error)
+        throw new ApolloError('query.validateCodebook error')
+      }
+    },
+    validateRawfileCodebookPair: async (obj, { rawDatasetIDRF, objectNameRF, rawDatasetIDCB, objectNameCB }, { driver, ogm }) => {
+      try {
+
+        const MinioUploadModel = ogm.model("MinioUpload")
+
+        const {bucketName:bucketNameRF, rawDataset:rawDatasetRF, datafileMinioUpload:datafileMinioUploadRF, result:csvRF} = await validateFile(ogm, rawDatasetIDRF, objectNameRF, 0, true)
+        const {bucketName:bucketNameCB, rawDataset:rawDatasetCB, datafileMinioUpload:datafileMinioUploadCB, result:csvCB} = await validateFile(ogm, rawDatasetIDCB, objectNameCB, 0, false)
+        let keysRF =  Object.keys(csvRF[0])
+        let keysCB = csvCB.map(x => x[0])
+
+        // keysRF.push('a') // test mismatches
+        // keysCB.push('b') // test mismatches
+
+        // keysRF.push('c') // test mismatches
+        // keysCB.push('c') // test mismatches
+
+        // keysRF.push('d') // test mismatches
+        // keysCB.push('e') // test mismatches
+
+        let isValid = true
+        let message = `Rawdata file and codebook match`
+        let mismatches = {}
+
+        if (keysRF.length !== keysCB.length) {
+          message = `Number of columns in Rawdata file (${keysRF.length}) does not match number of data variables in the codebook (${keysCB.length})`
+          isValid = false
+        } else {
+
+          keysRF.forEach((keyRF, index) => {
+            const keyCB = keysCB[index]
+            if (!(keyRF === keyCB)) {
+              mismatches[index] = [keyRF, keyCB]
+            }
+          })
+  
+          if (Object.keys(mismatches).length != 0) {
+            message = `Rawdata file does not match the codebook: ${JSON.stringify(mismatches)}`
+            isValid = false
+          }
+
+        }
+
+        const rawDatasetModelUpdateRF = await MinioUploadModel.update({ 
+          where: { objectName: datafileMinioUploadRF[0].objectName },
+          update: { pairedCodebook: {connect: {
+            where: {node: {objectName: datafileMinioUploadCB[0].objectName}},
+            edge: {validated: isValid},
+          }}},
+        })
+
+        const rawDatasetModelUpdateCB = await MinioUploadModel.update({ 
+          where: { objectName: datafileMinioUploadCB[0].objectName },
+          update: { pairedRawdataFile: {connect: {
+            where: {node: {objectName: datafileMinioUploadRF[0].objectName}},
+            edge: {validated: isValid},
+          }}},
+        })
+
+        return {isValid, message}
+      } catch (error) {
+        console.log(error)
+        throw new ApolloError('query.validateCodebook error')
+      }
+    },
   },
   Mutation: {
     minioUpload: async (obj, { bucketName, file }, { driver }) => {
