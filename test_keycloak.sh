b58aa48e90fc7690c7e675f972938ce3718b1c69
jfoong <justin.foong@uhn.ca>
2023-03-06 15:52:46 -0500

    Test keycloak

--- test_keycloak.sh
+++ test_keycloak.sh
@@ -9,3 +9,25 @@ echo $token;
 curl -s -k -g -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"query":"query{studies {studyID fullName}}"}' https://localhost:4001/graphql | jq
 
 # curl -s -k -g -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"query":"mutation { testKeycloak }"}' https://localhost:4001/graphql | jq
+
+curl -s -k -g -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"query":"query { studies { studyID } }"}' https://localhost:4001/graphql | jq
+
+
+# echo -e "\n\n\nbbb\n\n\n"
+
+
+# curl -s -k --request POST \
+#   --url https://localhost:4001/graphql \
+#   -H "Authorization: Bearer $token" \
+#   --header 'accept: application/json' \
+#   --header 'accept-encoding: gzip, deflate, br' \
+#   --header 'connection: keep-alive' \
+#   --header 'dnt: 1' \
+#   --header 'origin: http://localhost:4001/graphql' \
+#   --form 'operations={"query": "mutation minioUpload($file: Upload!) {minioUpload( bucketName: \"raw-dataset-7ec33aac-9209-4948-8804-8cc115bc8b20\" file: $file ) { bucketName objectName filename presignedURL  }}",   "variables": { "file": null } }' \
+#   --form 'map={ "nFile": ["variables.file"] }' \
+#   --form nFile=@/c/Users/jfoong/Downloads/rawdata_sample_4.csv.gz | jq
+
+
+
+  # "mutation minioUpload {minioUpload( bucketName: "bbb" file: "bbb" ) { bucketName objectName filename presignedURL  }}"
