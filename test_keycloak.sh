0066a7e8615a178a203e6e7cee34aa6119f56ee2
jfoong <justin.foong@uhn.ca>
2023-04-20 11:28:38 -0400

    Test a and b user

--- test_keycloak.sh
+++ test_keycloak.sh
@@ -6,6 +6,10 @@ token=`curl -k -s -d "client_secret='this should be a long secret'" -d 'client_i
 
 echo $token;
 
+token=`curl -k -s -d "client_secret='this should be a long secret'" -d 'client_id=pibu-app' -d 'username=b@b.b' -d 'password=b' -d 'grant_type=password' "https://${KEYCLOAK_SERVER_HOST_AND_PORT}/auth/realms/pibu/protocol/openid-connect/token" | jq -r .access_token`
+
+echo $token;
+
 curl -s -k -g -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"query":"query{studies {studyID fullName}}"}' https://${KEYCLOAK_SERVER_HOST_AND_PORT}/graphql | jq
 
 # curl -s -k -g -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"query":"mutation { testKeycloak }"}' https://localhost:4001/graphql | jq
