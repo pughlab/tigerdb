7127e01f5a49e4ee47e064939e6aaf66f1976086
jfoong <justin.foong@uhn.ca>
2023-05-12 14:31:48 -0400

    Auth token

--- get_keycloak_token.sh
+++ get_keycloak_token.sh
@@ -2,6 +2,6 @@
 
 source .env
 
-token=`curl -k -s -d "client_secret='this should be a long secret'" -d 'client_id=pibu-app' -d 'username=b@b.b' -d 'password=b' -d 'grant_type=password' "https://${KEYCLOAK_SERVER_HOST_AND_PORT}/auth/realms/pibu/protocol/openid-connect/token" | jq -r .access_token`
+token=`curl -k -s -d "client_secret='this should be a long secret'" -d 'client_id=pibu-app' -d 'username=a@a.a' -d 'password=a' -d 'grant_type=password' "https://${KEYCLOAK_SERVER_HOST_AND_PORT}/auth/realms/pibu/protocol/openid-connect/token" | jq -r .access_token`
 
 echo $token;
