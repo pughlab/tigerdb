98346d39074bfe68e31f16faba81edce668620a4
suluxan <suluxan.m@gmail.com>
2023-12-15 05:24:05 -0500

    about

--- ui/src/components/logos/index.tsx
+++ ui/src/components/logos/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import logo from './logo.png'
-import { Image, Container, Segment, Header, Icon, Divider, Grid } from 'semantic-ui-react'
+import { Image, Container, Message, Header, Segment, Divider, Grid } from 'semantic-ui-react'
 
 export function Logo ({size='medium', ... props}) {
     return (
@@ -16,10 +16,13 @@ export function AboutPortal ({}) {
           <Header as='h1'>
             {/* <Icon name='tint' color='red' size='big' /> */}
             <Header.Content>
-              TCRdb
+              TCR-DB: T-Cell Receptor Database
             </Header.Content>
           </Header>
         </Divider>
+        <Message>
+        TCR-DB is a scalable web portal powered by a graph database. Features include a TCR sequence search and functions for integrative analysis.
+        </Message>
       </Container>
     )
   }
