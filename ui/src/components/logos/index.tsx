70a956a37f7a55cca2b3a91cb32dc074c2a9e989
suluxan <suluxan.m@gmail.com>
2024-02-02 16:48:43 +0000

    TIGERdb changes

--- ui/src/components/logos/index.tsx
+++ ui/src/components/logos/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 
 import logo from './logo.png'
-import { Image, Container, Message, Header, Segment, Divider, Grid } from 'semantic-ui-react'
+import tigerdbLogo from './tigerdb-logo.png'
+import { Image, Container, Message, Header, Button, Divider, Grid } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 export function Logo ({size='medium', ... props}) {
     return (
@@ -16,13 +18,24 @@ export function AboutPortal ({}) {
           <Header as='h1'>
             {/* <Icon name='tint' color='red' size='big' /> */}
             <Header.Content>
-              TCR-DB: T-Cell Receptor Database
+              TIGERdb: T-cell and ImmunoGlobulin Epitope Receptor DataBase
             </Header.Content>
           </Header>
         </Divider>
-        <Message>
-        TCR-DB is a scalable web portal powered by a graph database. Features include a TCR sequence search and functions for integrative analysis.
+
+        <Message positive>
+        This website is free and open to all users.
         </Message>
-      </Container>
+        <Message attached color='grey' >
+        TIGERdb is a scalable web portal powered by a graph database. Features include a TCR sequence search and functions for integrative analysis.
+
+        </Message>
+        <Message attached='bottom'>
+          <Button as={Link} to={'/public/annotations'} fluid style={{backgroundColor: "#E8F3F0"}} >
+          <Image size='large' src={tigerdbLogo} centered />
+
+          </Button>
+        </Message>
+        </Container>
     )
   }
