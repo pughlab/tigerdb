1a447ac9c4ee5bb499e3666c2fac2cf69134e1ae
Jose Urbina <jurbinan@gmail.com>
2024-10-02 16:58:47 -0400

    getProjects resolver and type defs

--- api/src/graphql/resolvers/project.ts
+++ api/src/graphql/resolvers/project.ts
@@ -1,39 +1,92 @@
 import { ApolloError } from 'apollo-server'
-import { connect } from 'http2'
 
 export const resolvers = {
   Query: {
-
-  },
-  Mutation: {
-    createProject: async (
-      parent,
-      {name, description}, // missing data fill out here
-      { ogm, kauth }
-    ) => {
+    getProjects: async (obj, {}, {ogm, kauth})=> {
       try {
-
         const { sub: keycloakUserID } = kauth.accessToken.content
         const UserModel = ogm.model('KeycloakUser')
         const user = await UserModel.find({
-          where: {id: keycloakUserID}
+          where: {keycloakUserID: keycloakUserID}
         })
         if (!user) {
           throw new Error('User not found')
         }
-        
-        const ProjectModel = ogm.model('Project')
-        const projectInput = {name, description, public: false, sharedWith: []}
-        const {project} = await ProjectModel.create({ 
-          input: projectInput,
-          createdBy: {
-            connect: {
-              where: { node: { id: keycloakUserID }}
-            }
-          }
-        })
+        const filters = {
+          OR: [
+            { createdBy: user[0] },
+            { isPublic: true },
+            //{ sharedWith_IN: [user[0]] }
+          ]
+        }
+        // if (name) {
+        //   filters.name = name
+        // }
 
-        return project.records[0].get(0).properties 
+        // if (description) {
+        //   filters.description = description
+        // }
+
+        const ProjectModel = ogm.model('Project')
+        const selectionSet = `{
+          projectID
+          name
+          createdOn
+          isPublic
+          description
+          createdBy {
+            keycloakUserID
+            name
+            email
+          }
+        }`
+        ProjectModel.selectionSet = selectionSet
+        const projects = await ProjectModel.find({
+          where: filters,
+        }
+      )
+
+        return projects
+      } catch (error) {
+        console.log('getProjects', error)
+        throw new ApolloError('getProjects', error as string)
+      }
+    }
+  },
+  Mutation: {
+    createProject: async (
+      obj,
+      {name, description}, // missing data fill out here
+      { ogm, kauth }
+    ) => {
+      try {
+        const { sub: keycloakUserID } = kauth.accessToken.content
+        const UserModel = ogm.model('KeycloakUser')
+        const user = await UserModel.find({
+          where: {keycloakUserID: keycloakUserID}
+        })
+        if (!user) {
+          throw new Error('User not found')
+        }
+        const ProjectModel = ogm.model('Project')
+        const projectInput = {
+          name,
+          description,
+          isPublic: false,
+          sharedWith: [],
+          // datasets: [],
+          // runs: [],
+          createdBy: { 
+            connect: {
+              where: { node: { keycloakUserID: user[0].keycloakUserID }}
+            }
+        }}
+        const {projects: [project]} = await ProjectModel.create({ 
+          input: [projectInput]
+        })
+        console.log('Created project is:')
+        console.log(project)
+        return project//.records[0].get(0).properties 
       } catch (error) {
         console.log('createProject', error)
         throw new ApolloError('createProject', error as string)
@@ -41,14 +94,14 @@ export const resolvers = {
     },
     updateProject: async (
       parent,
-      {projectID, name, description, sharedWith, isPublic, datasets, runs},
+      {projectID, name, description, sharedWith, isPublic, datasets},
       {ogm, kauth}
     ) => {
       try {
         const { sub: keycloakUserID } = kauth.accessToken.content
         const UserModel = ogm.model('KeycloakUser')
         const user = await UserModel.find({
-          where: {id: keycloakUserID}
+          where: {keycloakUserID: keycloakUserID}
         })
         if (!user) {
           throw new Error('User not found')
@@ -58,7 +111,7 @@ export const resolvers = {
 
         const projectInput = { name, description, sharedWith, isPublic }
         const { project } = await ProjectModel.find({
-          where: { id: projectID }
+          where: { projectID: projectID }
         })
         if (!project) {
           throw new Error('Project not found')
@@ -87,7 +140,7 @@ export const resolvers = {
         const ProjectModel = ogm.model('Project')
 
         const { project } = await ProjectModel.find({
-          where: { id: projectID }
+          where: { projectID: projectID }
         })
         if (!project) {
           throw new Error('Project not found')
