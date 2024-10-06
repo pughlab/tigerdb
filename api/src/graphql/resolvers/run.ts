9a3413538df9de15082a3c658b4518d6a10f4619
suluxan <suluxan.m@gmail.com>
2024-10-06 06:10:43 -0400

    create run with minio bucket and connect to datasets (also getRuns by user query)

--- api/src/graphql/resolvers/run.ts
+++ api/src/graphql/resolvers/run.ts
@@ -1,52 +1,112 @@
+import { makeBucket } from '../../minio/minio';
 import { ApolloError } from "apollo-server";
 
 export const resolvers = {
-  Query: {},
-  Mutation: {
-    createRun: async (
-      parent,
-      { projectID, name, datasets, description, wesID },
-      { ogm, kauth }
-    ) => {
+  Query: {
+    getRuns: async (obj, {}, { ogm, kauth }) => {
       try {
         const { sub: keycloakUserID } = kauth.accessToken.content;
         const UserModel = ogm.model("KeycloakUser");
         const user = await UserModel.find({
-          where: { id: keycloakUserID },
+          where: { keycloakUserID: keycloakUserID },
         });
         if (!user) {
           throw new Error("User not found");
         }
 
-        const ProjectModel = ogm.model("Project");
-        const { project } = await ProjectModel.find({
-          where: { id: projectID },
+        const RunModel = ogm.model("Run");
+        const selectionSet = `{
+          runID
+          name
+          description
+          createdBy {
+            keycloakUserID
+            email
+            name
+          }
+          createdOn
+          status
+          datasets {
+            datasetID
+            name
+          }
+        }`;
+        RunModel.selectionSet = selectionSet;
+        const runs = await RunModel.find({
+          where: { createdBy: user[0] },
         });
 
-        if (!project) {
-          throw new Error("Project not found");
+        return runs;
+      } catch (error) {
+        console.log("getRuns", error);
+        throw new ApolloError("getRuns", error as string);
+      }
+    }
+
+
+
+  },
+  Mutation: {
+    createRunWithMinioBucket: async (
+      parent,
+      { name, description, datasetIDs },
+      { ogm, kauth, minioClient }
+    ) => {
+      try {
+        const { sub: keycloakUserID } = kauth.accessToken.content;
+        const UserModel = ogm.model("KeycloakUser");
+        const user = await UserModel.find({
+          where: {keycloakUserID: keycloakUserID}
+        });
+        if (!user) {
+          throw new Error("User not found");
         }
 
+        // const ProjectModel = ogm.model("Project");
+        // const { project } = await ProjectModel.find({
+        //   where: { id: projectID },
+        // });
+
+        // if (!project) {
+        //   throw new Error("Project not found");
+        // }
+
         const RunModel = ogm.model("Run");
-        const runInput = { description, name, wesID, status: "pending" };
-        const run = await RunModel.create({
-          input: runInput,
+        const runInput = { 
+          name, 
+          description,
           createdBy: {
             connect: {
-              where: { node: { id: keycloakUserID }}
+              where: { node: { keycloakUserID: user[0].keycloakUserID }}
             }
           },
           datasets: {
-            connect: datasets.map((dataset: any) => ({
-              where: { node: { dataset }}
+            connect: datasetIDs.map((datasetID: string) => ({
+              where: { node: { datasetID: datasetID }}
             }))
-          }
-        });
+          },
 
-        return run.records[0].get(0).properties
+          // should we connect to project?
+          // project: {
+          //   connect: {
+          //     where: { node: { projectID: projectID }}
+          //   }
+          // },
+        }
+        const {runs: [run]} = await RunModel.create({
+          input: [runInput],
+        })
+
+        const { runID } = run
+        const bucketName = `run-${runID}`
+        await makeBucket(minioClient, bucketName)
+
+        console.log('Created run is:')
+        console.log(run)
+        return run //.records[0].get(0).properties
       } catch (error) {
-        console.log("createRun", error);
-        throw new ApolloError("createRun", error as string);
+        console.log("createRunWithMinioBucket", error);
+        throw new ApolloError("createRunWithMinioBucket", error as string);
       }
     },
     updateRun: async (
