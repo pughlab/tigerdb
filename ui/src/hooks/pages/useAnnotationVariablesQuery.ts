9dbdb929426c8f17a849f5f58bfeab3cd255e9bc
suluxan <suluxan.m@gmail.com>
2024-10-22 06:01:46 -0400

    hook to get both curatedAnnotations and curatedDatasets

--- ui/src/hooks/pages/useAnnotationVariablesQuery.ts
+++ ui/src/hooks/pages/useAnnotationVariablesQuery.ts
@@ -1,89 +1,80 @@
-// import { gql, useMutation, useQuery, useLazyQuery } from '@apollo/client'
-// import { useCallback, useEffect, useMemo, useReducer, useState } from 'react'
-
-
-
-// export default function useDataVariablesQuery({ }) {
-
-//   const [loadQuery, { data, loading, error, called }] = useLazyQuery(gql`
-//         query DataVariables($curatedDatasetIDs: [ID!]!, $dataVariableWhere: DataVariableWhere) {
-//             curatedDatasets(
-//                 where: {OR: [{curatedDatasetID_IN: $curatedDatasetIDs}]}, 
-//             ) {
-//                 curatedDatasetID
-//                 name
-//                 dataVariables(
-//                     where: $dataVariableWhere
-//                     # where: {OR: [{chromosome:"chr1",start_GTE:10000,end_LTE: 50000, datavalue_GTE: 0.1},
-//                     # {chromosome:"chr2",start_GTE:50000,end_LTE: 500000, datavalue_GTE: 0.2},
-//                     # {chromosome:"chr2",start_GTE:500000,end_LTE: 5000000, datavalue_GTE: 0.2},
-//                     # {chromosome:"chr3",start_GTE:5000000,end_LTE: 50000000, datavalue_GTE: 0.3}]}
-//                     # options:{limit:10000, sort: [{ chromosome: ASC },{ start: ASC }]}
-//                     options:{limit:50000}
-//                 ) {
-//                     dataVariableID
-//                     chromosome
-//                     start
-//                     end
-//                     datavalue
-//                 }  
-//             }
-//         }
-//   `, { variables: { curatedDatasetIDs, dataVariableWhere }, fetchPolicy: 'network-only' })
-
-//   return {state, dispatch, data, loading, error, called, loadQuery}
-// }
-
-
-
-
-import { gql, useMutation, useLazyQuery } from '@apollo/client'
+import { gql, useLazyQuery } from '@apollo/client'
 import { useCallback, useState } from 'react'
 
-export default function useAnnotationVariablesQuery(dropdownFilters: any) {
+export default function useAnnotationAndDatasetVariablesQuery(dropdownFilters: any) {
   const [searchText, setSearchText] = useState('')
 
-  const constructWhereClause = (filters: any) => {
+  const constructWhereClause = (filters: any, allowedKeys: string[] = []) => {
     let where = {};
     Object.entries(filters).forEach(([key, value]) => {
       if (Array.isArray(value) && value.some(item => item !== null)) {
         // Filter out null values from the array
         const filteredValues = value.filter(item => item !== null);
-        if (filteredValues.length > 0) {
-          where = {...where, [`${key}_IN`]: filteredValues}
+        if (filteredValues.length > 0 && (allowedKeys.length === 0 || allowedKeys.includes(key))) {
+          where = { ...where, [`${key}_IN`]: filteredValues };
         }
       }
     });
-    console.log(where)
     return where;
   };
 
-  const where = constructWhereClause(dropdownFilters);
-  const [runQuery, { loading, data, error, called }]  = useLazyQuery(gql`
-  query AnnotationVariables($where: AnnotationVariableWhere) {
-    curatedAnnotations {
-      curatedAnnotationID
-      name
-      description
-      annotationVariables(where: $where) 
-      {
-        annotationVariableID
-        locus
-        cdr3b 
-        trbv
-        trbj
-        mhc
-        mhcClass
-        epitope
-        epitopeGene
-        epitopeSpecies
-        reference
+  // Construct where clauses
+  const annotationWhere = constructWhereClause(dropdownFilters); // All filters for annotations
+  const datasetWhere = constructWhereClause(dropdownFilters, ['cdr3b', 'trbv', 'trbj']); // Only specific fields for datasets
+
+  // const where = constructWhereClause(dropdownFilters);
+
+  const [runQuery, { loading, data, error, called }] = useLazyQuery(gql`
+    query AnnotationAndDatasetVariables($annotationWhere: AnnotationVariableWhere, $datasetWhere: DatasetVariableWhere) {
+      curatedAnnotations {
+        dataset {
+          datasetID
+          name
+          project {
+            projectID
+            name
+          }
+        }
+        curatedAnnotationID
+        annotationVariables(where: $annotationWhere) {
+          annotationVariableID
+          locus
+          cdr3b 
+          trbv
+          trbj
+          mhc
+          mhcClass
+          epitope
+          epitopeGene
+          epitopeSpecies
+          reference
+        }
+      }
+      curatedDatasets {
+        dataset {
+          datasetID
+          name
+          project {
+            projectID
+            name
+          }
+        }
+        curatedDatasetID
+        datasetVariables(where: $datasetWhere) {
+          datasetVariableID
+          cdr3b
+          trbv
+          trbj
+          # cdr3a
+          # subjectcondition
+          # count
+        }
       }
     }
-  }`, { 
-    variables: {where: where},
-    fetchPolicy: 'network-only' 
+  `, {
+    variables: { annotationWhere, datasetWhere },
+    fetchPolicy: 'network-only'
   })
-  
+
   return { data, loading, error, called, searchText, setSearchText, runQuery }
 }
