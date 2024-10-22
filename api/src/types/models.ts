392beb826f4e4df403ebc8d8cfc26e0591d4f999
suluxan <suluxan.m@gmail.com>
2024-10-22 05:53:47 -0400

    debugging dataSources but probably not needed

--- api/src/types/models.ts
+++ api/src/types/models.ts
@@ -1,9 +1,15 @@
 import { Client } from "minio";
 import { Driver } from "neo4j-driver";
+import { OGM } from "@neo4j/graphql-ogm";
+import { WesAPI } from "../graphql/wesAPI"; // Assuming you have WesAPI imported correctly
 
 export type MyContextType = {
   driver: Driver
   neo4jDatabase: string
   minioClient: Client
   kauth: any
+  ogm: OGM; // Include OGM instance
+  dataSources: {
+    wesAPI: WesAPI; // Include dataSources explicitly
+  };
 }
