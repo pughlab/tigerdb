ecbb449245a1f4b89670f8f8d7b884429c819a9b
suluxan <suluxan.m@gmail.com>
2026-05-29 00:33:48 +0200

    redesign tcr card to look like annotation card

--- ui/src/components/pages/Runs/graph/TCRInfo.tsx
+++ ui/src/components/pages/Runs/graph/TCRInfo.tsx
@@ -94,81 +94,73 @@ export default function TCRInfo({ node }: Readonly<{ node: any }>) {
           }}
           onClick={() => {openDocument(data.reference, data.projectID)}
         }>
-          <Label attached={"top right"} color="black">{node.source}</Label>
-          <Grid columns={3} divided textAlign={"center"}>
+          <Label attached={"top right"} style={{ backgroundColor: node.color || 'black', color: 'white' }}>{node.source}</Label>
+          <Grid textAlign="center">
             <Grid.Row>
               <Grid.Column>
-                <List.Header as={Header} color={"blue"}>
+                <List.Header as={Header} style={{ color: node.color || '#2185d0' }}>
                   <div>{node.label}</div>
                 </List.Header>
               </Grid.Column>
             </Grid.Row>
-            <Grid.Row>
-              <Grid.Column><strong>Node ID:</strong> {node.id}</Grid.Column>
-              <Grid.Column><strong>Community:</strong> {node.group}</Grid.Column>
-              <Grid.Column><strong>TRBV:</strong> {node.v_gene}</Grid.Column>
+            <Grid.Row columns={5} divided textAlign="center">
+              <Grid.Column>
+                <Grid.Row>
+                  <Label basic content="TRBV" detail={`${node.v_gene ? node.v_gene : 'NA'}`} color="grey" />
+                </Grid.Row>
+                <Grid.Row>
+                  <Label basic content="TRBJ" detail={`${data.j_gene ? data.j_gene : 'NA'}`} color="grey" />
+                </Grid.Row>
+                <Grid.Row>
+                  <Label basic content="TRAV" detail={`${data.trav ? data.trav : 'NA'}`} color="grey" />
+                </Grid.Row>
+                <Grid.Row>
+                  <Label basic content="TRAJ" detail={`${data.traj ? data.traj : 'NA'}`} color="grey" />
+                </Grid.Row>
+              </Grid.Column>
+              
+              <Grid.Column>
+                <Grid.Row>
+                  <Label basic content="MHC" detail={`${data.mhc ? data.mhc : 'NA'}`} color="grey" />
+                </Grid.Row>
+                <Grid.Row>
+                  <Label basic content="MHC Class" detail={`${data.mhcClass ? data.mhcClass : 'NA'}`} color="grey" />
+                </Grid.Row>
+              </Grid.Column>
+              
+              <Grid.Column>
+                <Grid.Row>
+                  <Label basic content="Epitope Amino Acid Sequence" detail={`${data.epitopeAAseq ? data.epitopeAAseq : 'NA'}`} color="grey" />
+                </Grid.Row>
+                <Grid.Row>
+                  <Label basic content="Epitope Gene" detail={`${data.epitopeGene ? data.epitopeGene : 'NA'}`} color="grey" />
+                </Grid.Row>
+                <Grid.Row>
+                  <Label basic content="Epitope Species" detail={`${data.epitopeSpecies ? data.epitopeSpecies : 'NA'}`} color="grey" />
+                </Grid.Row>
+              </Grid.Column>
+              
+              <Grid.Column>
+                <Grid.Row>
+                  <Label basic content="Mutation" detail={`${data.mutation ? data.mutation : 'NA'}`} color="grey" />
+                </Grid.Row>
+                <Grid.Row>
+                  <Label basic content="Recognizes Wild Type Epitope" detail={`${data.recognizesWTEpitope ? data.recognizesWTEpitope : 'NA'}`} color="grey" />
+                </Grid.Row>
+              </Grid.Column>
+              
+              <Grid.Column>
+                <Grid.Row>
+                  <Label basic content="UniProt" detail={`${data.uniProt ? data.uniProt : 'NA'}`} color="grey" />
+                </Grid.Row>
+                {/* <Grid.Row>
+                  <Label basic content="Node ID" detail={`${node.id}`} color="grey" />
+                </Grid.Row> */}
+                <Grid.Row>
+                  <Label basic content="Community" detail={`${node.group}`} color="grey" />
+                </Grid.Row>
+              </Grid.Column>
             </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>{data.projectName.length > 0 && (
-                <>
-                  <strong>Project:</strong> {data.projectName}
-                </>
-                )}
-              </Grid.Column>
-              <Grid.Column>
-                {data.datasetName.length > 0 && (
-                  <>
-                    <strong>Dataset:</strong> {data.datasetName}
-                  </>
-                )}
-              </Grid.Column>
-              <Grid.Column>
-                {data.trav.length > 0 && (
-                  <>
-                    <strong>TRAV:</strong> {data.trav}
-                  </>
-                )}
-              </Grid.Column>
-            </Grid.Row>
-            {(data.j_gene || data.traj) && (
-              <Grid.Row>
-              <Grid.Column>
-                {data.j_gene.length > 0 && (
-                  <>
-                    <strong>Subject Condition:</strong> {data.j_gene}
-                  </>
-                )}
-              </Grid.Column>
-              <Grid.Column>
-                {data.traj.length > 0 && (
-                  <>
-                    <strong>Clone Count:</strong> {data.traj}
-                  </>
-                )}
-              </Grid.Column>
-              <Grid.Column></Grid.Column>
-            </Grid.Row>
-            )}
-            {(data.mhc || data.mhcClass || data.epitopeGene) && (
-              <Grid.Row>
-                <Grid.Column><strong>MHC:</strong> {data.mhc || 'NA'}</Grid.Column>
-                <Grid.Column><strong>MHC Class:</strong> {data.mhcClass || 'NA'}</Grid.Column>
-                <Grid.Column><strong>Epitope Gene:</strong> {data.epitopeGene || 'NA'}</Grid.Column>
-              </Grid.Row>
-            )}
-            {(data.epitopeAAseq || data.epitopeSpecies || data.mutation) && (
-              <Grid.Row>
-                <Grid.Column><strong>Epitope AA Sequence:</strong> {data.epitopeAAseq || 'NA'}</Grid.Column>
-                <Grid.Column><strong>Epitope Species:</strong> {data.epitopeSpecies || 'NA'}</Grid.Column>
-                <Grid.Column><strong>Mutation:</strong> {data.mutation || 'NA'}</Grid.Column>
-              </Grid.Row>
-            )}
-            {(data.recognizesWTEpitope || data.uniProt) && (
-              <Grid.Row>
-                <Grid.Column><strong>Recognizes Wild Type Epitope:</strong> {data.recognizesWTEpitope || 'NA'}</Grid.Column>
-                <Grid.Column><strong>UniProt:</strong> {data.uniProt || 'NA'}</Grid.Column>
-              </Grid.Row>
-            )}
           </Grid>
           <Divider horizontal hidden />
           {referenceButton}
